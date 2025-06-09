5cd6c26d2bcd9ca31a0d1b3c44532059648783d0
SalvatoreGiacomazzo16 <salvatore161giacomazzo@gmail.com>
2025-06-09 19:53:32 +0200

    start button toggle logic

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 /* Start button behavior*/
 const startButton = document.getElementById('start-button');
+const gameGrid = document.getElementById('game-grid');
+const startContainer = document.getElementById('start-container');
+
+startButton.addEventListener('click', () => {
+    startContainer.classList.add('d-none');
+    gameGrid.classList.remove('d-none');
+});

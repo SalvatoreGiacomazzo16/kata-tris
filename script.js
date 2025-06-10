b2fab25e139b75f36a9a7ea63405fa513d92b2ce
SalvatoreGiacomazzo16 <salvatore161giacomazzo@gmail.com>
2025-06-10 21:20:55 +0200

    player handling start

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const startButton = document.getElementById('start-button');
 const startScreen = document.getElementById('start-screen');
 const gameGrid = document.getElementById('game-grid');
-const singleCell = document.querySelector('.cell');
+const singleCell = document.querySelectorAll('.cell');
 
 
-
+//Start Button handling
 startButton.addEventListener('click', () => {
     startScreen.classList.add('d-none');
     gameGrid.classList.remove('d-none');
-
 });
+
+//Players handling
+const playerOne = 'X';
+const playerTwo = 'O';
+let currentPlayer = playerOne;

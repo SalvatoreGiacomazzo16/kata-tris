16519bac235f0bedeac8cd3c52ffa26394e7b5f2
SalvatoreGiacomazzo16 <salvatore161giacomazzo@gmail.com>
2025-06-16 09:47:46 +0200

    finished exercise, handled win and draw

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const gameGrid = document.getElementById('game-grid');
 const cells = document.querySelectorAll('.cell');
 const playerText = document.getElementById('currentPlayer');
 const winnerText = document.getElementById('winner');
-
+const restartButton = document.getElementById('restart-button');
 
 //Start Button handling
 startButton.addEventListener('click', () => {
@@ -41,4 +41,59 @@ function handleCellClick(event) {
     }
     //Turn for player on screen
     playerText.textContent = 'Turn for : ' + currentPlayer;
+
+    //Stop the game if there's a winner
+
+    if (checkWin(currentPlayer)) {
+        winnerText.textContent = `${currentPlayer} WINS!`;
+
+        cells.forEach(cell => cell.removeEventListener('click', handleCellClick));
+        return;
+    }
+
+    //Draw checking
+    if (checkDraw()) {
+        winnerText.textContent = "DRAW";
+        return;
+    }
+
+
+
+    // Draw handling
+
+    function checkDraw() {
+        return [...cells].every(cell => cell.textContent === 'X' || cell.textContent === 'O');
+    }
+
 }
+
+//Winning conditions
+const winCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+
+function checkWin(player) {
+    for (let i = 0; i < winCombinations.length; i++) {
+        const [a, b, c] = winCombinations[i];
+        if (
+            cells[a].textContent === player &&
+            cells[b].textContent === player &&
+            cells[c].textContent === player
+        ) {
+            return true;
+        }
+    }
+    return false;
+}
+//Refresh button
+restartButton.addEventListener('click', () => {
+    location.reload();
+});
